82dd8d78319a74e25b022a5b6f6c9d55408dd5c7
Artur Ribeiro <artur.felipe.ribeiro2010@gmail.com>
2024-11-17 21:47:53 -0300

    feat: add browse router

--- ProjetoFinal/projeto_pweb/src/index.js
+++ ProjetoFinal/projeto_pweb/src/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route exact path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
